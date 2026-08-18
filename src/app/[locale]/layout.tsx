be9289d6295d4c189e1ab6638a0208639511b732
MyLabAdmin <mylab.admin@gmail.com>
2026-08-18 23:47:08 +0200

    feat: integrate brand typography

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,8 +1,19 @@
 import type { ReactNode } from 'react'
+import { Geist, Geist_Mono } from 'next/font/google'
 import { notFound } from 'next/navigation'
 import { NextIntlClientProvider } from 'next-intl'
 import { getMessages } from 'next-intl/server'
 import { routing } from '@/i18n/routing'
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+})
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+})
 
 type LocaleLayoutProps = {
   children: ReactNode
@@ -28,7 +39,7 @@ export default async function LocaleLayout({
     <html
       lang={locale}
       dir={direction}
-      className="h-full antialiased"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
         <NextIntlClientProvider locale={locale} messages={messages}>
@@ -38,3 +49,4 @@ export default async function LocaleLayout({
     </html>
   )
 }
+
